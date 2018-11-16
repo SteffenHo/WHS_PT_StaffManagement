be03761909db46fd9582e2346e3b4323e5687320
Steffen Holtkamp <stef.holt@yahoo.de>
2018-11-16 10:20:50 +0100

    Add implementation for employee

---
+++ src/main/java/de/PT/personal/Employee.java
@@ -0,0 +1,18 @@
+package de.PT.personal;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Employee extends AbstractEmployee {
+
+    public Employee(String name, double salary, Date dateOfEmployment, int id) {
+        super(name, salary, dateOfEmployment, id);
+    }
+
+    @Override
+    public boolean equals(Object testEmployee) {
+        if (this == testEmployee) return true;
+        if (!(testEmployee instanceof Employee)) return false;
+        return  super.equals(testEmployee);
+    }
+}
