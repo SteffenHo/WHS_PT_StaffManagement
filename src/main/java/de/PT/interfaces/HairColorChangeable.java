a8b4c06c6a23c50a5e3da93e88baad089dce201a
Steffen Holtkamp <stef.holt@yahoo.de>
2018-11-30 10:12:40 +0100

    Add interface hair

---
+++ src/main/java/de/PT/interfaces/HairColorChangeable.java
@@ -0,0 +1,17 @@
+package de.PT.interfaces;
+
+public interface HairColorChangeable {
+
+    /**
+     * Get the hair color
+     * @return String color
+     */
+    String getHairColor();
+
+    /**
+     * Set the color of the hair
+     * @param color String
+     * @return String new color
+     */
+    String setHairColor(String color);
+}
