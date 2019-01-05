2680c29375333f931849fa139fe363bddcb7f4b7
Steffen Holtkamp <stef.holt@yahoo.de>
2019-01-05 20:47:02 +0100

    Add consts for gui table description

---
+++ src/main/java/de/PT/utils/Const.java
@@ -0,0 +1,8 @@
+package de.PT.utils;
+
+public class Const {
+    public static String[] getColumnNames(){
+        String[] column =  {"Id", "Name", "Gehalt", "Datum", "Kleidergroesse", "istManager", "Bonus"};
+        return column;
+    }
+}
